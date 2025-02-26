75e74cb2319fb4467923956998587317dcfe7c80
@dedospelados <apaganotto006@gmail.com>
2025-02-26 00:13:58 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_Manager_CLI_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateNow = DateTime.Today;
+            Console.WriteLine(dateNow);
+        }
+    }
+}
