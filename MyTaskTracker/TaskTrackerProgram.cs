95217531704013817b064acc9a7e246f6be2a699
@dedospelados <apaganotto006@gmail.com>
2025-06-17 23:04:07 -0300

    Rearranged folders and tried to parse a JSON

---
+++ MyTaskTracker/TaskTrackerProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace Task_Manager_CLI_01
+{
+
+    public class Racistas
+    {
+        public string? Name { get; set; }
+
+        public int RaceNumber { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+
+    public class TaskTrackerProgram
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Racistas ElGoat = new Racistas
+            {
+                Name = "Max",
+                RaceNumber = 33,
+                DateTime = DateTime.UtcNow
+            };
+
+            Console.WriteLine("clave?");
+
+            string JsonifiedObj = JsonSerializer.Serialize(ElGoat);
+
+        }
+    }
+}
